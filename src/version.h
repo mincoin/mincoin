9498fec841455f1d282b06d257a11d6304c5629f
SandyCohen <ralphlova@hotmail.com>
2013-05-28 11:54:14 +1000

    version change for hard fork

--- src/version.h
+++ src/version.h
@@ -14,7 +14,7 @@
 // These need to be macro's, as version.cpp's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       6
-#define CLIENT_VERSION_REVISION    3
+#define CLIENT_VERSION_REVISION    4
 #define CLIENT_VERSION_BUILD       0
 
 static const int CLIENT_VERSION =
@@ -31,7 +31,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60001;
+static const int PROTOCOL_VERSION = 60002;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
