57a3360eb21698bd63f4dae7794f89429ebef4b3
Xi Eta <xieta@mincointools.com>
2018-11-25 06:16:32 +0800

    Mincoin: Set Client Version to MincoinCore

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -14,7 +14,7 @@
  * for both bitcoind and bitcoin-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("MincoinCore");
 
 /**
  * Client version number
