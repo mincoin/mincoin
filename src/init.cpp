0f159454129ea3257682895e5a40f60d0b72f8cb
Xi Eta <xieta@mincointools.com>
2018-11-25 06:14:41 +0800

    Mincoin: Rename IPC Threads

--- src/init.cpp
+++ src/init.cpp
@@ -185,7 +185,7 @@ void Shutdown()
     /// for example if the data directory was found to be locked.
     /// Be sure that anything that writes files or flushes caches only does this if the respective
     /// module was initialized.
-    RenameThread("bitcoin-shutoff");
+    RenameThread("mincoin-shutoff");
     mempool.AddTransactionsUpdated(1);
 
     StopHTTPRPC();
@@ -569,7 +569,7 @@ void CleanupBlockRevFiles()
 void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
 {
     const CChainParams& chainparams = Params();
-    RenameThread("bitcoin-loadblk");
+    RenameThread("mincoin-loadblk");
     CImportingNow imp;
 
     // -reindex
