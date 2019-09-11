9e256fd5eb7d17a8903f989e3b88dda4b1f547fb
Xi Eta <xieta@mincointools.com>
2019-09-11 11:09:14 +0800

    Mincoin: Set Base58 Prefix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -123,9 +123,9 @@ public:
         vSeeds.push_back(CDNSSeedData("mincointools.com", "seed.mincointools.com", true)); // Xi Eta
         vSeeds.push_back(CDNSSeedData("mincoinpool.org", "seed.mincoinpool.org", true)); // Xi Eta
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,50);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,178);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
