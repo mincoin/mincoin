f5436b9bb3624e7d2227b770e9234b040adfb5d8
Xi Eta <xieta@mincointools.com>
2019-09-11 10:48:30 +0800

    Mincoin: Set Dates, Names and URIs
    - Set Copyright Date and Holders
    - Set Repository and Website Locations
    - Update Qt Organization and Application Details

--- src/clientversion.h
+++ src/clientversion.h
@@ -23,10 +23,10 @@
 #define CLIENT_VERSION_IS_RELEASE true
 
 /**
- * Copyright year (2009-this)
+ * Copyright year (2013-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2017
+#define COPYRIGHT_YEAR 2019
 
 #endif //HAVE_CONFIG_H
 
@@ -38,7 +38,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " " COPYRIGHT_HOLDERS_FINAL
+#define COPYRIGHT_STR "2013-" STRINGIZE(COPYRIGHT_YEAR) " " COPYRIGHT_HOLDERS_FINAL
 
 /**
  * bitcoind-res.rc includes this file, but it cannot cope with real c++ code.
