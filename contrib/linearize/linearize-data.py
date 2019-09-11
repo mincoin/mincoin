117e82eca48d98db0ea43f4657e7b128f1e2d743
Xi Eta <xieta@mincointools.com>
2019-09-11 11:06:24 +0800

    Mincoin: Update Linearize Script

--- contrib/linearize/linearize-data.py
+++ contrib/linearize/linearize-data.py
@@ -283,9 +283,9 @@ if __name__ == '__main__':
 	settings['rev_hash_bytes'] = settings['rev_hash_bytes'].lower()
 
 	if 'netmagic' not in settings:
-		settings['netmagic'] = 'f9beb4d9'
+		settings['netmagic'] = '6342212c'
 	if 'genesis' not in settings:
-		settings['genesis'] = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+		settings['genesis'] = '12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2'
 	if 'input' not in settings:
 		settings['input'] = 'input'
 	if 'hashlist' not in settings:
